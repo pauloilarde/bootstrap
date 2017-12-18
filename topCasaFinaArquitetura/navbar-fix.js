39b03a44a3a1c132815abe99c2482c2c206fc7d5
Paulo <pauloilarde@gmail.com>
2017-12-18 19:14:56 -0200

    Fix na animação do navbar

---
+++ topCasaFinaArquitetura/navbar-fix.js
@@ -0,0 +1,7 @@
+$('#collapse-navbar').on('show.bs.collapse', function () {
+    $('.topCasaFina-banner').css('transform', 'translate(-50%, 30%)');
+});
+
+$('#collapse-navbar').on('hide.bs.collapse', function () {
+    $('.topCasaFina-banner').css('transform', 'translate(-50%, -50%)');
+});
